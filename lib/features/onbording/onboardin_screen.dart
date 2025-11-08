465ac85d4b4eb72920430b7d56254b76d71800ff
osama-41 <osama.tarefee123@gmail.com>
2025-11-08 16:02:11 +0300

    -Added some colors & styles

---
+++ lib/features/onbording/onboardin_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class OnboardinScreen extends StatelessWidget {
+  const OnboardinScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
